68bda87374ea78e0511439c3fc9ee459c02b136a
troy96475 <taesupark0504@gmail.com>
2024-08-13 23:26:47 +0900

    first commit

---
+++ src/main/java/com/example/daedong_dayt/DaedongDaytApplication.java
@@ -0,0 +1,13 @@
+package com.example.daedong_dayt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaedongDaytApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaedongDaytApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/daedong_dayt/DaedongDaytApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daedong_dayt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaedongDaytApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
